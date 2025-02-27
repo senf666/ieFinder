8b5717c1af813da62d195c7ed508bbbe63df38ff
D <3512672+DannyCork@users.noreply.github.com>
2025-02-27 00:27:43 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# iefinder
+
+# .ie finder
+
+.ie finder is a python based application that finds and logs `.ie` domain names by checking certificate transparency logs in near real-time.
+
+<img  alt="image"  src="screenshot.png">
+
+
+## Features
+
+- Logs `.ie` domain names to text files.
+- Generates logs in multiple formats: `.txt`, `.csv`.
+- Provides verbose output for all domains passing through using --verbose flag.
+- Displays hyperlinks to the found `.ie` domains.
+
+## Requirements
+
+- Python 3.x
+
+# Installation
+
+```bash
+$ python3  -m  venv  .venv
+$ source  .venv/bin/activate
+$ pip  install  -r  requirements.txt
+```
+
+  
+
+# Usage
+```bash
+$$ python3 ieFinder.py
+
+    _         _____  _             _             
+   (_)  ___  |  ___|(_) _ __    __| |  ___  _ __ 
+   | | / _ \ | |_   | || '_ \  / _` | / _ \| '__|
+ _ | ||  __/ |  _|  | || | | || (_| ||  __/| |   
+(_)|_| \___| |_|    |_||_| |_| \__,_| \___||_|   
+                                                 
+ The .ie domain name finder | Github
+
+[2025-02-27 00:04:05] old.idpd.ecoserve.ie
+[2025-02-27 00:04:09] www.icecompanyinc.ie
+[2025-02-27 00:05:06] qjcmekguojdoy8w.mail.yt.m3dia.ie
+[2025-02-27 00:05:11] quantumfulfilment.ie
+[2025-02-27 00:05:11] *.tiltinsurance.ie
+[2025-02-27 00:05:12] help.apexit.ie
+[2025-02-27 00:05:14] draft.kmk.ie
+[2025-02-27 00:05:39] *.ruby-consulting.ie
+[2025-02-27 00:06:13] driveinc.ie
+[2025-02-27 00:06:23] www.autoconfig.avalon-house.ie
+[2025-02-27 00:06:36] www.autoconfig.avalon-house.ie
+[2025-02-27 00:06:37] *.ruby-consulting.ie
+...
+```
+
+# Output
+.ie Finder produces 4 log files:
+```
+
+├── log.txt       contains a log of what happened.
+├── certstream.txt certstream log.
+├── domains.txt   contains .ie domain names.
+└── www.txt       contains .ie domain names without subdomains, just www.domainName.ie
+```
---
+++ ieFinder.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+import logging
+import sys
+import certstream
+import argparse
+import time
+from art import *
+from termcolor import cprint, colored
+import sys
+import datetime
+from colorama import init,Fore,Back,Style
+import sys
+import os
+
+logging.basicConfig(filename='logs/certstream.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+init(autoreset=True)
+
+parser=argparse.ArgumentParser(description='Finds and logs .ie domain names by checking certfiicate transparency logs in near realtime.')
+parser.add_argument('-v','--verbose',help='verbose output (All domains passing through).',required=False,action='store_true')
+args=vars(parser.parse_args())
+
+# -------------------------------------------------------------------------------
+
+def on_error(exception):
+    logging.error(f"Exception in CertStreamClient! -> {exception}")
+
+# -------------------------------------------------------------------------------
+
+def write_to_files(domain,clean,www):
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    with open(os.path.join('logs', 'log.txt'), 'a') as f:
+        f.write(domain+"\n")
+    with open(os.path.join('logs', 'domains.txt'), 'a') as f:
+        f.write(clean+"\n")
+    with open(os.path.join('logs', 'domains.csv'), 'a') as f:
+        f.write(f'{timestamp},{clean}\n')
+    with open(os.path.join('logs', 'www.txt'), 'a') as f:
+        f.write(f'{www}\n')
+
+# -------------------------------------------------------------------------------
+
+def print_callback(message, context):
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+    if message['message_type']=="heartbeat":
+        return
+    if message['message_type']=="certificate_update":
+        all_domains=message['data']['leaf_cert']['all_domains']
+
+        if len(all_domains)==0:
+            domain="NULL"
+        else:
+            domain=all_domains[0]
+
+            if args['verbose'] is True:
+                print (f'{Style.DIM}[{timestamp}]: {domain}')
+            
+            if domain.endswith(".ie"):                           
+                clean_domain_name = domain.split('.')[len(domain.split('.'))-2] + '.' + domain.split('.')[len(domain.split('.'))-1]
+                www_domain_name = 'www.' + domain.split('.')[len(domain.split('.'))-2] + '.' + domain.split('.')[len(domain.split('.'))-1]
+                write_to_files(domain,clean_domain_name,www_domain_name)
+                url = f'https://{domain}'
+                print (f'{Style.DIM}[{timestamp}]{Style.RESET_ALL}{Fore.GREEN} {hyperlink(url,domain)}')
+ 
+        sys.stdout.flush()
+
+# -------------------------------------------------------------------------------
+
+def hyperlink(uri, label=None):
+    if label is None: 
+        label = uri
+    
+    parameters = ''
+    escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
+    return escape_mask.format(parameters, uri, label)
+
+# -------------------------------------------------------------------------------
+
+def main():
+    cprint(text2art(".ie   Finder"), "green",end = " ")
+    print(f'{Fore.GREEN}The .ie do{Fore.WHITE}main na{Fore.LIGHTYELLOW_EX}me finder{Fore.RESET}{Style.DIM} | {hyperlink("https://github.com/senf666/iefinder","Github")}\n')
+
+    certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
+
+# -------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    main()
